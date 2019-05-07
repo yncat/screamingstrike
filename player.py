49b17fdbe9775a4889002703c835230981e85cb5
nyanchan <personal@nyanchangames.com>
2019-05-07 15:18:28 +0900

    docstrings for player

--- player.py
+++ player.py
@@ -14,11 +14,17 @@ DEFAULT_PUNCH_RANGE = 4
 DEFAULT_PUNCH_SPEED = 200
 
 class Player():
+	"""This class represents a player."""
 	def __init__(self):
 		pass
 	def __del__(self):
 		pass
 	def initialize(self,field):
+		"""
+Initializes this player.
+:param field: The field instance on which this player should be bound.
+:type field: GameField
+"""
 		self.field=field
 		self.lives=3
 		self.x=field.getCenterPosition()
@@ -41,6 +47,7 @@ class Player():
 		self.penetrate=False
 
 	def frameUpdate(self):
+		"""Call this method once per frame to keep everything updated."""
 		self.consecutiveHitBonus.frameUpdate()
 		self.consecutiveMissUnbonus.frameUpdate()
 		if self.punching is False and globalVars.appMain.wnd.keyPressed(window.K_SPACE): self.punchLaunch()
@@ -51,6 +58,8 @@ class Player():
 			if not elem.frameUpdate(): self.itemEffects.remove(elem)
 
 	def punchLaunch(self):
+		"""Launches a punch. If this player is already punching, this method does nothing. """
+		if self.punching: return
 		self.punches+=1
 		self.punching=True
 		s=sound()
@@ -61,6 +70,7 @@ class Player():
 		self.punchTimer.restart()
 
 	def punchHit(self):
+		"""Process punch hits. Called from frameUpdate as it is needed."""
 		self.punching=False
 		hit=0
 		for pos in range(int(self.punchRange)+1):
@@ -94,11 +104,13 @@ class Player():
 	# end punchHit
 
 	def punchMiss(self):
+		"""Processes punch miss. Called from punchHit."""
 		self.consecutiveMisses+=1
 		if self.hits>0: self.calcHitPercentage()
 		self.processConsecutiveHits()
 
 	def processConsecutiveHits(self):
+		"""Processes consecutive hits bonus. Called from punchHit."""
 		if not self.field.modeHandler.allowConsecutiveHitsBonus: return
 		if self.consecutiveHits>5:
 			self.field.log(_("%(hits)d consecutive hits bonus!") % {"hits": self.consecutiveHits})
@@ -108,6 +120,7 @@ class Player():
 		self.consecutiveHits=0
 
 	def processConsecutiveMisses(self):
+		"""Processes consecutive misses penalty. Called from punchHit."""
 		if not self.field.modeHandler.allowConsecutiveMissesBonus: return
 		if self.consecutiveMisses>5:
 			self.field.log(_("%(misses)d consecutive misses penalty!") % {"misses": self.consecutiveMisses})
@@ -117,12 +130,22 @@ class Player():
 		self.consecutiveMisses=0
 
 	def processItemHit(self,it):
+		"""
+Processes hitting an item. Called from punchHit. This method just checks the item is good or nasty, then calls the specific methods.
+:param it: Item instance which got hit.
+:type it: Item
+"""
 		if it.type==itemConstants.TYPE_NASTY:
 			self.processNastyItemHit(it)
 		else:
 			self.processGoodItemHit(it)
 
 	def processNastyItemHit(self,it):
+		"""
+Processes a nasty item hit. Called from processItemHit.
+:param it: Item instance which got hit.
+:type it: Item
+"""
 		if it.identifier==itemConstants.NASTY_SHRINK:
 			e=itemEffects.ShrinkEffect()
 			e.initialize(self)
@@ -143,6 +166,11 @@ class Player():
 			return
 
 	def processGoodItemHit(self,it):
+		"""
+Processes a good item hit. Called from processItemHit.
+:param it: Item instance which got hit.
+:type it: Item
+"""
 		if it.identifier==itemConstants.GOOD_MEGATONPUNCH:
 			existing=self.findEffect("Megaton punch")
 			if existing is None:
@@ -182,21 +210,42 @@ class Player():
 			return
 
 	def findEffect(self,name):
+		"""
+Checks whether this player has the specified effect currently active.
+:param name: Name of the effect to search(Blurred, Megaton Punch, etc).
+:type name: str
+:rtype: bool
+"""
 		for elem in self.itemEffects:
 			if elem.name==name: return elem
 		return None
 
 	def setPunchRange(self,r):
+		"""
+Changes this player's effective punch length.
+:param r: New range.
+:type r: int
+"""
 		previous=self.punchRange
 		self.field.log(_("The effective range of your Punch is now %(range)d (from %(from)d)") % {"range": r, "from": previous})
 		self.punchRange=r
 
 	def setPunchSpeed(self,s):
+		"""
+Changes this player's punching speed.
+:param s: New speed in milliseconds.
+:type s: int
+"""
 		previous=self.punchSpeed
 		self.field.log(_("The speed of your punch is now %(speed)d milliseconds (from %(from)d)") % {"speed": s, "from": previous})
 		self.punchSpeed=s
 
 	def setPenetration(self,p):
+		"""
+Sets whether this player's punches penetrate.
+:param p: Penetrate?
+:type p: bool
+"""
 		if p is True:
 			self.field.log(_("Your punches now penetrate enemies and items!"))
 		else:
@@ -204,8 +253,15 @@ class Player():
 		self.penetration=p
 
 	def calcHitPercentage(self):
+		"""Calculates the hitting percentage of this player and updates the hitPercentage property."""
 		self.hitPercentage=self.hits/self.punches*100
+
 	def moveTo(self,p):
+		"""
+Moves this player to the specified position.
+:param p: New position.
+:type p: int
+"""
 		self.x=p
 		s=sound()
 		s.load(globalVars.appMain.sounds["change.ogg"])
@@ -213,6 +269,7 @@ class Player():
 		s.play()
 
 	def hit(self):
+		"""Called when this player gets hit by one of the enemies."""
 		self.lives-=1
 		self.field.log(_("You've been slapped! (%(lives)d HP remaining)") % {"lives": self.lives})
 		s=sound()
@@ -225,6 +282,11 @@ class Player():
 			s.play()
 
 	def addScore(self,score):
+		"""
+Add a specified amount of score to this player.
+:param score: Score to add.
+:type score: int
+"""
 		self.score+=score
 		which=_("added")
 		if score<=0: which=_("subtracted")
