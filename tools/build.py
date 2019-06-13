541a3023ebeaf73342124cf936aa7212180ccf77
nyanchan <personal@nyanchangames.com>
2019-06-13 21:05:59 +0900

    Started editing build script for pyinstaller

--- tools/build.py
+++ tools/build.py
@@ -12,7 +12,7 @@ def dopackage():
 	if win:
 		print("Creating installer exe")
 		f=open("_build.bat","w")
-		f.write("WinRAR a -cfg- -ed -ep1 -k -m5 -r -sfx \"-ztools\\rar_options.txt\" \"%s.exe\" \"%s.dist\\*\"" % (PROJECT_FULL_NAME, PROJECT))
+		f.write("WinRAR a -cfg- -ed -ep1 -k -m5 -r -sfx \"-ztools\\rar_options.txt\" \"%s.exe\" \"dist\\%s\\*\"" % (PROJECT_FULL_NAME, PROJECT))
 		f.close()
 		common.run("cmd /c _build.bat")
 		os.remove("_build.bat")
@@ -46,22 +46,20 @@ if "--skip-compile" in sys.argv:
 copydir=""
 
 if win:
-	cmd="nuitka --follow-imports --windows-disable-console --standalone --mingw64 --include-plugin-directory=sound_lib/lib/x86 %s.py" % (PROJECT)
-	copydir="%s.dist" % PROJECT
+	cmd="pyinstaller --onefile --windowed %s.py" % (PROJECT)
+	copydir="dist/%s" % PROJECT
 else:
 	cmd="pyinstaller --windowed --onefile --osx-bundle-identifier com.nyanchanGames.%s %s.py" % (PROJECT_FULL_NAME, PROJECT)
 	copydir="dist/%s.app/Contents/Resources" % PROJECT
 
 common.run(cmd, sh=win)#win uses shell=true and mac doesn't
-if win:
-	print("Copying pythoncom37.dll...")
-	shutil.copyfile("%s/Lib/site-packages/pywin32_system32/pythoncom37.dll" % (PYTHON_PATH), "%s.dist/pythoncom37.dll" % PROJECT)
-
-print("Copying sound_lib dlls...")
-shutil.copytree("sound_lib/lib/x64", "%s/sound_lib/lib/x64" % copydir)
-common.mkdir("%s/accessible_output2" % copydir)
-print("Copying accessible_output2 dlls...")
-shutil.copytree("accessible_output2/lib", "%s/accessible_output2/lib" % copydir)
+if not win:
+	print("Copying sound_lib dlls...")
+	shutil.copytree("sound_lib/lib", "%s/sound_lib/lib" % copydir)
+	common.mkdir("%s/accessible_output2" % copydir)
+	print("Copying accessible_output2 dlls...")
+	shutil.copytree("accessible_output2/lib", "%s/accessible_output2/lib" % copydir)
+#end copies for mac
 print("Copying data files...")
 shutil.copytree("data", "%s/data" % (copydir),
                 ignore=shutil.ignore_patterns("options.dat","collection.dat","stats.dat"))
@@ -79,6 +77,6 @@ for elem in changelogs:
 
 if win:
 	print("Renaming to play.exe")
-	os.rename(PROJECT+".dist/"+PROJECT+".exe",PROJECT+".dist/play.exe")
+	os.rename(copydir+"/"+PROJECT+".exe",copydir+"/play.exe")
 dopackage()
 print("Done!")
