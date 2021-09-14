59442bdc04951c1aaac9260e46390be3eaec5748
Yukio Nozawa <personal@nyanchangames.com>
2021-09-14 22:50:48 +0900

    Add the same shrink safeguard to burden mode

--- gameModes.py
+++ gameModes.py
@@ -190,11 +190,20 @@ class BurdenModeHandler(ModeHandlerBase):
 	def spawnItem(self):
 		spd=random.randint(100,800)
 		t=itemConstants.TYPE_NASTY
-		ident=random.randint(0,itemConstants.NASTY_MAX)
+		ident=self.selectNastyItem()
 		i=item.Item()
 		i.initialize(self.field,random.randint(0,self.field.x-1),spd,t,ident)
 		self.field.items.append(i)
 		self.resetItemComingTimer()
+
+	def selectNastyItem(self):
+		"""Prevents shrink from appearing when the player already has 3 shrink effects."""
+		shrinks = len([e for e in self.field.player.itemEffects if e.name == itemConstants.NAMES[itemConstants.TYPE_NASTY][itemConstants.NASTY_SHRINK]])
+		while(True):
+			ret = random.randint(0,itemConstants.NASTY_MAX)
+			if ret == itemConstants.NASTY_SHRINK and shrinks==3: continue
+			break
+		return ret
 
 	def resetItemComingTimer(self):
 		self.itemComingTimer.restart()
