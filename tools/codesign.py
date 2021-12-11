b6f7178bcaa7ec50aa9168d2a75e9d95d3f37517
nyanchan <personal@nyanchangames.com>
2021-12-11 15:21:21 +0900

    Remove experimental codesign script

--- tools/codesign.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-cmdlist = ["codesign", "-f", "-s", "Yukio Nozawa", "-v", "--deep", "--timestamp", "--entitlements", "entitlements.plist", "-o", "runtime", "dist/ss.app"]
-subprocess.run(cmdlist)
--- tools/codesign.sh
+++
@@ -1,1 +0,0 @@
-codesign -f -s "Yukio Nozawa" -v --deep --timestamp --entitlements entitlements.plist -o runtime "dist/ss.app"
