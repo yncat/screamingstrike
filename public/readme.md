b23c1b10aca464440774e79188ccf65ae625e67e
nyanchan <personal@nyanchangames.com>
2021-12-11 13:48:49 +0900

    Rewrite build script

---
+++ public/readme.md
@@ -0,0 +1,48 @@
+# Screaming Strike  
+## Intro
+I decided to opensource this.  
+
+## Dependencies  
+Windows: pip install -r requirements-win.txt  
+Mac: pip3 install -r requirements-mac.txt
+
+## Running from source
+Windows: python ss.py  
+Mac: python3 ss.py  
+
+## Other useful commands  
+Note: If You're using mac, you need to use / instead of \.  
+python tools\build.py  
+Builds the app on win and mac.  Please read the following building section for details.  
+python tools\updateTranslation.py  
+Updates the translation catalog. Currently, cannot be executed on mac. Please read the following translating section for details.  
+python tools\buildTranslation.py  
+Compiles your edited translation catalogs. Currently, it cannot be executed on mac.  
+python tools\initdoc.py  
+Initializes auto document creation. Please read the auto documentation section for details.  
+python build\gendoc.py  
+to generate the API document.  
+
+## Building
+The game uses pyinstaller for building.  
+For automated packaging, WinRAR must be installed and WinRAR.exe must be accessible from the environment variable path.  
+On mac OSX, you just need pyinstaller.  
+After setting up the environment, run 'python tools\build.py'.  
+
+## Translating
+Sorry, it works only on Windows right now.  
+First, create folder for your language in the locale folder (E.G. ja-JP).  
+Run 'python tools\updateTranslation.py'  
+Edit the catalog file in the lc_messages folder under your created folder.  
+Run 'python tools\buildTranslation.py'  
+Your added language should be selectable from the options menu.  
+
+## API reference
+You need sphinx.  
+pip install sphinx  
+run the following commands to generate the API reference, which may help you how the game is coded. There's missing docstrings here and there, so a small part of the API reference may have less detailed info.  
+python tools\initdoc.py  
+python tools\gendoc.py  
+
+## Contributing
+Please post issues / suggestions / ideas to the issue tracking in this repository. If you did something interesting, feel free to send pull requests.  
--- tools/build.py
+++ tools/build.py
@@ -1,29 +1,88 @@
 # -*- coding: utf-8 -*-
 # app build tool
-# Copyright (C) 2019 Yukio Nozawa <personal@nyanchangames.com>
+# Copyright (C) 2019-2021 Yukio Nozawa <personal@nyanchangames.com>
 import os
 import platform
 import sys
 import shutil
+import subprocess
 import glob
 import common
+
+
+def clearPreviousBuild():
+    if os.path.isdir("build"):
+        shutil.rmtree("build")
+    if os.path.isdir("dist"):
+        shutil.rmtree("dist")
+
+
+def copyBeforeBuild():
+    os.mkdir("build")
+    for file in glob.glob("*.*"):
+        shutil.copyfile(file, os.path.join("build", file))
+    deletefiles = [
+        "requirements-win.txt",
+        "requirements-mac.txt",
+        "result.txt"
+    ]
+    for file in deletefiles:
+        os.remove(os.path.join("build", file))
+    copydirs = [
+        "data",
+        "locale",
+        "public",
+        "sound_lib",
+        "platform_utils",
+        "libloader",
+        "accessible_output2"
+    ]
+    for dir in copydirs:
+        shutil.copytree(dir, os.path.join("build", dir))
+    shutil.copyfile(os.path.join("tools", "rar_options.txt"),
+                    os.path.join("build", "rar_options.txt"))
+
+
+def removeUnnecessaryFilesForMac():
+    shutil.rmtree("build/sound_lib/lib/x86")
+    libs = glob.glob("build/sound_lib/lib/x64/*.dll")
+    libs.extend(glob.glob("build/sound_lib/lib/x64/*.so"))
+    for lib in libs:
+        os.remove(lib)
+    shutil.rmtree("build/accessible_output2/lib")
 
 
 def dopackage():
     if win:
         print("Creating installer exe")
         f = open("_build.bat", "w")
-        f.write("WinRAR a -cfg- -ed -ep1 -k -m5 -r -sfx \"-ztools\\rar_options.txt\" \"%s.exe\" \"dist\\%s\\*\"" % (PROJECT_FULL_NAME, PROJECT))
+        f.write("WinRAR a -cfg- -ed -ep1 -k -m5 -r -sfx \"-zrar_options.txt\" \"%s.exe\" \"dist\\%s\\*\"" %
+                (PROJECT_FULL_NAME, PROJECT))
         f.close()
         common.run("cmd /c _build.bat")
         os.remove("_build.bat")
     if not win:
         print("codesigning...")
-        os.rename("dist/" + PROJECT + ".app", "dist/" + PROJECT_FULL_NAME + ".app")
-        os.remove("dist/%s" % PROJECT)
-        common.run("codesign -s \"Yukio Nozawa\" -v --deep --timestamp --entitlements entitlements.plist -o runtime \"dist/%s.app\"" % PROJECT_FULL_NAME)
+        subprocess.run([
+            "codesign",
+            "-f",
+            "-s",
+            "Yukio Nozawa",
+            "-v",
+            "--deep",
+            "--timestamp",
+            "--entitlements",
+            "entitlements.plist",
+            "-o",
+            "runtime",
+            "dist/%s.app" % PROJECT
+        ])
+        os.rename(os.path.join("dist", PROJECT + ".app"),
+                  os.path.join("dist", PROJECT_FULL_NAME+".app"))
+        os.remove(os.path.join("dist", PROJECT))
         print("Creating image dmg")
-        common.run("hdiutil create -volname %s -srcfolder ./dist -ov -format UDZO %s.dmg" % (PROJECT_FULL_NAME, PROJECT_FULL_NAME))
+        common.run("hdiutil create -volname %s -srcfolder ./dist -ov -format UDZO %s.dmg" %
+                   (PROJECT_FULL_NAME, PROJECT_FULL_NAME))
 
 
 win = True
@@ -47,45 +106,37 @@ if not os.path.exists("data"):
     print("Error: no data folder found. Your working directory must be the root of the project. You shouldn't cd to tools and run this script.")
     sys.exit()
 
-print("Building %s. This will take several minutes. Please wait..." % PROJECT)
-
-if "--skip-compile" in sys.argv:
-    print("Skipping to packaging")
-    dopackage()
-    print("Done!")
-    sys.exit()
-
 copydir = ""
+
+clearPreviousBuild()  # Copy all files to build directory first
+copyBeforeBuild()
+if not win:
+    removeUnnecessaryFilesForMac()
+
+os.chdir("build")
 
 if win:
     cmd = "pyinstaller --windowed %s.py" % (PROJECT)
     copydir = "dist/%s" % PROJECT
 else:
-    cmd = "pyinstaller --windowed --onefile --osx-bundle-identifier com.nyanchanGames.%s %s.py" % (PROJECT_FULL_NAME, PROJECT)
+    cmd = "pyinstaller --windowed --onefile --osx-bundle-identifier com.nyanchanGames.%s %s.py" % (
+        PROJECT_FULL_NAME, PROJECT)
     copydir = "dist/%s.app/Contents/Resources" % PROJECT
 
 common.run(cmd, sh=win)  # win uses shell=true and mac doesn't
 if not win:
-    print("Copying sound_lib dlls...")
+    print("Copying sound_lib dynamic libraries...")
     shutil.copytree("sound_lib/lib", "%s/sound_lib/lib" % copydir)
-    common.mkdir("%s/accessible_output2" % copydir)
-    print("Copying accessible_output2 dlls...")
-    shutil.copytree("accessible_output2/lib", "%s/accessible_output2/lib" % copydir)
 # end copies for mac
 print("Copying data files...")
 shutil.copytree("data", "%s/data" % (copydir),
                 ignore=shutil.ignore_patterns("options.dat", "collection.dat", "stats.dat"))
 print("Copying locale files...")
 shutil.copytree("locale", "%s/locale" % (copydir))
-print("Copying readme")
-readmes = glob.glob("readme_*.txt")
-for elem in readmes:
-    shutil.copyfile(elem, "%s/%s" % (copydir, elem))
-
-print("Copying changelog")
-changelogs = glob.glob("changelog_*.txt")
-for elem in changelogs:
-    shutil.copyfile(elem, "%s/%s" % (copydir, elem))
+print("Copying other public files")
+for file in glob.glob("public/*.*"):
+    basename = os.path.basename(file)
+    shutil.copyfile(file, os.path.join(copydir, basename))
 
 if win:
     print("Renaming to play.exe")
