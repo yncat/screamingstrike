a2ca6dca88f6fe25002f18c15cedb25926db31b0
nyanchan <personal@nyanchangames.com>
2021-12-12 22:51:36 +0900

    Add after_notarization tool

---
+++ tools/after_notarization.py
@@ -0,0 +1,34 @@
+import os
+import common
+
+PROJECT_FULL_NAME = "screamingStrike2"
+
+os.chdir("build")
+
+common.run([
+    "xcrun",
+    "stapler",
+    "staple",
+    "dist/%s.app" % (PROJECT_FULL_NAME)
+])
+
+common.run([
+    "hdiutil",
+    "create",
+    "-volname",
+    PROJECT_FULL_NAME,
+    "-srcfolder",
+    "./dist",
+    "-ov",
+    "-format",
+    "UDZO",
+    "%s.dmg" % (PROJECT_FULL_NAME)
+])
+
+common.run([
+    "codesign",
+    "-s",
+    "Yukio Nozawa",
+    "--timestamp",
+    "%s.dmg" % (PROJECT_FULL_NAME)
+])
